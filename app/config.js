896a216d4b25c55ad939f9669ab69a42fffc5b26
Anthony Tietjen <tietjen.anthony@gmail.com>
2017-02-25 07:48:53 -0700

    Cleanup

--- app/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.SESSION_SECRET = "abcdefghijklmnopqrstuvwxyz";
-exports.CONNECTION_PORT_RELEASE = 80;
-exports.CONNECTION_PORT_DEBUG = 3000;
-exports.CONNECTION_HOSTNAME_RELEASE = "127.0.0.1";
-exports.CONNECTION_HOSTNAME_DEBUG = "127.0.0.1";
